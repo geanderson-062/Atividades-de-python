0b779fb5e061771bb1675beebf1498bd2072fea4
geanderson062 <83518241+geanderson-062@users.noreply.github.com>
2023-07-03 21:26:16 -0300

    novos aprendizados

---
+++ Exemplos/exemplo_de_grafico_simples.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+
+# Dados do gráfico
+x = [-3, 0]
+y = [0, 1]
+
+# Criar o gráfico de linha
+plt.plot(x, y)
+
+# Adicionar rótulos aos eixos
+plt.xlabel("Eixo X")
+plt.ylabel("Eixo Y")
+
+# Adicionar um título ao gráfico
+plt.title("Gráfico de Linha")
+
+# Exibir o gráfico
+plt.show()
