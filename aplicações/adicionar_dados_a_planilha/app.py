361910d5a9aeab3b7d9c80f08ee8b95b048c4284
geanderson-062 <83518241+geanderson-062@users.noreply.github.com>
2024-01-21 00:11:28 -0300

    tabela

--- aplicações/adicionar_dados_a_planilha/app.py
+++ aplicações/adicionar_dados_a_planilha/app.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import ttk  # Importação adicional
 import customtkinter
 from tkinter import filedialog
 import openpyxl
@@ -14,19 +15,28 @@ customtkinter.set_appearance_mode("dark")
 customtkinter.set_default_color_theme("green")
 
 
+def adicionar_dados():
+    # Adiciona dados à tabela
+    dados = [id_entry.get(), cliente_entry.get(), produto_entry.get(), data_entry.get()]
+    tabela.insert("", "end", values=dados)
+    # Limpa as entradas
+    id_entry.delete(0, tk.END)
+    cliente_entry.delete(0, tk.END)
+    produto_entry.delete(0, tk.END)
+    data_entry.delete(0, tk.END)
+
+
 def salvar_excel():
-    # Coletando os dados
-    dados = {
-        "ID": id_entry.get(),
-        "Cliente": cliente_entry.get(),
-        "Produto": produto_entry.get(),
-        "Data": data_entry.get(),
-    }
+    # Coletando os dados da tabela
+    dados_salvos = []
+    for child in tabela.get_children():
+        dados_salvos.append(tabela.item(child)["values"])
     # Criando o workbook e adicionando os dados
     workbook = openpyxl.Workbook()
     sheet = workbook.active
-    sheet.append(list(dados.keys()))  # Cabeçalhos
-    sheet.append(list(dados.values()))  # Valores
+    sheet.append(["ID", "Cliente", "Produto", "Data"])  # Cabeçalhos
+    for item in dados_salvos:
+        sheet.append(item)  # Valores
     # Pedindo ao usuário para escolher onde salvar o arquivo
     file_path = filedialog.asksaveasfilename(defaultextension=".xlsx")
     if file_path:
@@ -35,7 +45,7 @@ def salvar_excel():
 
 # Ajustes de centralização para o grid
 root.grid_rowconfigure(0, weight=1)
-root.grid_rowconfigure(6, weight=1)
+root.grid_rowconfigure(8, weight=1)
 root.grid_columnconfigure(0, weight=1)
 root.grid_columnconfigure(3, weight=1)
 
@@ -56,11 +66,21 @@ produto_entry.grid(row=4, column=1, columnspan=2, pady=5)
 data_entry = customtkinter.CTkEntry(root, placeholder_text="Data")
 data_entry.grid(row=5, column=1, columnspan=2, pady=5)
 
+# Botão Adicionar
+add_button = customtkinter.CTkButton(root, text="Adicionar", command=adicionar_dados)
+add_button.grid(row=6, column=1, columnspan=2, pady=5)
+
 # Botão para salvar os dados em Excel
 save_button = customtkinter.CTkButton(
     root, text="Salvar como Excel", command=salvar_excel
 )
-save_button.grid(row=6, column=1, columnspan=2, pady=5)
+save_button.grid(row=7, column=1, columnspan=2, pady=5)
 
-# Executa a aplicação
+# Criando a tabela
+colunas = ("ID", "Cliente", "Produto", "Data")
+tabela = ttk.Treeview(root, columns=colunas, show="headings")
+for col in colunas:
+    tabela.heading(col, text=col)
+tabela.grid(row=8, column=1, columnspan=2, pady=10)
+
 root.mainloop()
